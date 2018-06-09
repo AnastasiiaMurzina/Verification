07c9c9774c5442bf54a43e81b0802f208538a86d
AnastasiiaMurzina <hloshkin@mail.ru>
2018-06-09 20:25:04 +0300

    Create example1.py

---
+++ example1.py
@@ -0,0 +1,17 @@
+def function1(b):
+    print('1')
+    return 0
+    print('0')
+
+def function2():
+    for i in range(10):
+        print(i, 'b')
+        if i == 5:
+            continue
+            print('after')
+
+def main1():
+    f = 2 * a
+    function1(f)
+    function2()
+
